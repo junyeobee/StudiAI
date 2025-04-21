a47dfaa38dbb04e4692b94bfa9a9aafa69b181d1
DESKTOP-DINGRG3\Junhyup <narosong@gmail.com>
2025-04-21 15:24:07 +0900

    feat(notion): 학습 계획 DB 자동 생성 + AI 요약 블록 구조 설계 및 구현

---
+++ notion.py
@@ -0,0 +1,57 @@
+import requests
+import json
+
+NOTION_TOKEN = "ntn_C58327611023N5xC6lu9H8zMIAuC2rmL225aGylmNQdbC7"
+NOTION_VERSION = "2022-06-28"
+
+headers = {
+    "Authorization": f"Bearer {NOTION_TOKEN}",
+    "Content-Type": "application/json",
+    "Notion-Version": NOTION_VERSION
+}
+
+def query_database(database_id):
+    url = f"https://api.notion.com/v1/databases/{database_id}/query"
+    res = requests.post(url, headers=headers)
+    print(res.status_code)
+    print(res.json())
+
+# 예시: 데이터베이스 생성 (상위 페이지 안에)
+def create_database(parent_page_id):
+    url = "https://api.notion.com/v1/databases"
+    payload = {
+        "parent": {
+            "type": "page_id",
+            "page_id": parent_page_id
+        },
+        "title": [
+            {
+                "type": "text",
+                "text": {
+                    "content": "학습 목표: React"
+                }
+            }
+        ],
+        "properties": {
+            "날짜": { "date": {} },
+            "학습 제목": { "title": {} },
+            "복습 여부": { "checkbox": {} },
+            "진행 상태": {
+                "select": {
+                    "options": [
+                        { "name": "시작 전", "color": "gray" },
+                        { "name": "진행중", "color": "blue" },
+                        { "name": "완료", "color": "green" }
+                    ]
+                }
+            }
+        }
+    }
+
+    res = requests.post(url, headers=headers, data=json.dumps(payload))
+    print(res.status_code)
+    print(res.json())
+
+
+query_database("1d215ba8df0e804abe7fd559f7ab6884")
+create_database("15415ba8df0e80e4834bcf738689aecc")
