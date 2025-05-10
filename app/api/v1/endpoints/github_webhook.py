57011713af439308b466078d6fd91ee380af42db
DESKTOP-DINGRG3\Junhyup <narosong@gmail.com>
2025-05-10 13:28:12 +0900

    feat: githubwebhook

---
+++ app/api/v1/endpoints/github_webhook.py
@@ -0,0 +1,23 @@
+
+
+
+# 깃허브 웹훅 설정
+async def get_git_hub_token() -> str:
+    # mcp가 깃허브 설정 페이지 안내 후 -> callback url에서 토큰 받아오는 함수
+
+    return str
+
+# 깃허브 토큰 저장
+async def set_token(token: str) -> str:
+    # 토큰 저장
+    return str
+
+# 깃허브 웹훅 설정
+async def set_github_webhook() -> str:
+    # 깃허브 웹훅 설정
+    return str
+
+# 웹훅 이벤트 처리
+async def get_github_webhook_event() -> str:
+    # 웹훅 이벤트 처리
+    return str
