c454278cd53bdc1f7c2560177afebf8ba92bb0a5
DESKTOP-DINGRG3\Junhyup <narosong@gmail.com>
2025-05-20 15:36:52 +0900

    refactor:testing

--- app/api/v1/endpoints/github_webhook.py
+++ app/api/v1/endpoints/github_webhook.py
@@ -147,15 +147,13 @@ async def handle_github_webhook(
                 for b in code_bundle:
                     commit_detail = await github_service.fetch_commit_detail(owner, repo, b["sha"])
                     for file in commit_detail["files"]:
-                        lines = []
-                        for line in file["patch"].splitlines():
-                            if line.startswith(("@@", "diff ", "index ", "--- ", "+++ ")):
-                                continue                 # 메타 제거
-                            if line.startswith(("+", "-")):
-                                line = line[1:]          # +/-
-                            lines.append(line.lstrip("\t"))  # 탭 제거
-                        clean = "\n".join(lines).rstrip()
-                        print(clean)    
+                        clean = re.sub(
+                            r"(?m)^(@@.*?$|[\+\-]\s?|diff --git.*$|index .*|--- .*|\+\+\+ .*)",
+                            "",
+                            file["patch"]
+                        ).rstrip()
+                        print(clean)
+                        
             case _:
                 pass
             
