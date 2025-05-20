9864f7455dd14933247ea0b5d1ac396bf59c7e8f
DESKTOP-DINGRG3\Junhyup <narosong@gmail.com>
2025-05-20 15:24:42 +0900

    refactor: 응답 개선중

--- app/api/v1/endpoints/github_webhook.py
+++ app/api/v1/endpoints/github_webhook.py
@@ -149,10 +149,10 @@ async def handle_github_webhook(
                     for file in commit_detail["files"]:
                         if file["status"] == "modified":
                             print(file["filename"])
-                            print(re.sub(r"(?m)^(\d*\+|\+)\s+", "", file["patch"]))
+                            print(re.sub(r"(?m)^(\d*\+|\+)\s+|\t", "", file["patch"]))
                         if file["status"] == "added":
                             print(file["filename"])
-                            print(re.sub(r"(?m)^(\d*\+|\+)\s+", "", file["patch"]))
+                            print(re.sub(r"(?m)^(\d*\+|\+)\s+|\t", "", file["patch"]))
 
                         
             case _:
